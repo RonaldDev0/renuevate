c2868554f4fc47518cdb896a34f7b43c0c57bebf
Ronald <ronaldzamoradev@gmail.com>
2023-06-06 15:15:04 -0500

    fix: sideMenu

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -3,7 +3,7 @@ import LinkItem from './LinkItem'
 
 export default function SideMenu () {
   return (
-    <div className='flex flex-col gap-3 fixed right-2 h-screen justify-center m-2'>
+    <div className='flex flex-col gap-3 fixed right-2 justify-center mx-2 mt-96'>
       {
         links.map(({ icon, path }: link) => <LinkItem key={path} path={path} icon={icon} />)
       }
